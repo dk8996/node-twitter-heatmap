5980e743d7c19bb59faa453ab4c9a213649db3ed
Dimitry Kudryavtsev <dkudryavstev@dataxu.com>
2013-07-27 04:18:52 -0400

    more changes to the code.

--- src/server/app.js
+++ src/server/app.js
@@ -6,35 +6,35 @@
 var fridge = require('./fridge');
 var credentials = require('./credentials.js');
 
-var twit = new twitter({
-    consumer_key: credentials.consumer_key,
-    consumer_secret: credentials.consumer_secret,
-    access_token_key: credentials.access_token_key,
-    access_token_secret: credentials.access_token_secret
-});
-
-twit.stream('statuses/sample', function(stream) {
-	stream.on('data', function(data) {
-		console.log(util.inspect(data));
-		io.sockets.emit('update', req.body);
-	});
-});
-
-
-function handler(req, res) {
-//	fs.readFile('index.html', function(err, data) {
-//		res.writeHead(200, {
-//			'Content-Type' : 'text/html'
-//		});
-//		res.end(data);
+//var twit = new twitter({
+//    consumer_key: credentials.consumer_key,
+//    consumer_secret: credentials.consumer_secret,
+//    access_token_key: credentials.access_token_key,
+//    access_token_secret: credentials.access_token_secret
+//});
+//
+//twit.stream('statuses/sample', function(stream) {
+//	stream.on('data', function(data) {
+//		console.log(util.inspect(data));
+//		io.sockets.emit('update', req.body);
 //	});
-        res.send(200);
-};
-
-io.sockets.on('connection', function (socket) {
-                console.log("Someone connected");
-});
-
-http.listen(8081, function() {
-	console.log('%s listening at %s', server.name, server.url);
-});
+//});
+//
+//
+//function handler(req, res) {
+////	fs.readFile('index.html', function(err, data) {
+////		res.writeHead(200, {
+////			'Content-Type' : 'text/html'
+////		});
+////		res.end(data);
+////	});
+//        res.send(200);
+//};
+//
+//io.sockets.on('connection', function (socket) {
+//                console.log("Someone connected");
+//});
+//
+//http.listen(8081, function() {
+//	console.log('%s listening at %s', server.name, server.url);
+//});
