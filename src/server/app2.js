b1c583d56e6414ade6db94b40599a634d6011fbe
Dimitry Kudryavtsev <dk8996@gmail.com>
2013-08-18 13:39:30 -0400

    Updating the server.

--- src/server/app2.js
+++ src/server/app2.js
@@ -15,48 +15,48 @@ console.log(credentials.credentials.consumer_key);
 
 
 var tu = require('tuiter')(keys);
-tu.filter({locations: [{lat: 41.1, long: -71.7},{lat: 42.8, long: -70}]}, function(stream){
-
-  // New tweet
-  stream.on("tweet", function(data){
-    console.log(data.text);
-    console.log(data.entities);
-    console.log(data.coordinates);
-    console.log(data.place.full_name);
-    console.log(data.place.bounding_box.coordinates);
-    
-    if(data.coordinates && data.coordinates.coordinates){
-      io.sockets.emit("update", {
-          coordinates: data.coordinates.coordinates
-        , screen_name: data.user.screen_name
-        , text: data.text
-        , profile_image_url: data.user.profile_image_url
-        , media_url: data.entities.media 
-      });
-    } else if(data.place){
-      var place = data.place.bounding_box.coordinates[0][0];
-       io.sockets.emit("update", {
-          coordinates: place
-        , screen_name: data.user.screen_name
-        , text: data.text
-        , profile_image_url: data.user.profile_image_url
-        , media_url: data.entities.media
-      });
-    }
-  });
-
-  stream.on("delete", function(data){
-    console.log("delete");
-    //I don't care about deleted tweets
-  });
-
-  // Log errors
-  stream.on("error", function(error){
-    // handle errors
-    console.log("error:"+error);
-  });
-
-});
+//tu.filter({locations: [{lat: 41.1, long: -71.7},{lat: 42.8, long: -70}]}, function(stream){
+//
+//  // New tweet
+//  stream.on("tweet", function(data){
+//    console.log(data.text);
+//    console.log(data.entities);
+//    console.log(data.coordinates);
+//    console.log(data.place.full_name);
+//    console.log(data.place.bounding_box.coordinates);
+//    
+//    if(data.coordinates && data.coordinates.coordinates){
+//      io.sockets.emit("update", {
+//          coordinates: data.coordinates.coordinates
+//        , screen_name: data.user.screen_name
+//        , text: data.text
+//        , profile_image_url: data.user.profile_image_url
+//        , media_url: data.entities.media 
+//      });
+//    } else if(data.place){
+//      var place = data.place.bounding_box.coordinates[0][0];
+//       io.sockets.emit("update", {
+//          coordinates: place
+//        , screen_name: data.user.screen_name
+//        , text: data.text
+//        , profile_image_url: data.user.profile_image_url
+//        , media_url: data.entities.media
+//      });
+//    }
+//  });
+//
+//  stream.on("delete", function(data){
+//    console.log("delete");
+//    //I don't care about deleted tweets
+//  });
+//
+//  // Log errors
+//  stream.on("error", function(error){
+//    // handle errors
+//    console.log("error:"+error);
+//  });
+//
+//});
 
 tu.filter({locations: [{lat: 36, long: -122},{lat: 38, long: -123}]}, function(stream){
 
